fdb76266f0dbe6d2201f664d1945e4f669224eec
Denil Chungath <95201442+denil-ct@users.noreply.github.com>
2023-06-13 01:46:46 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ import PackageDescription
 let package = Package(
     name: "swift-openapi-urlsession",
     platforms: [
-        .macOS(.v11), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
     ],
     products: [
         .library(
