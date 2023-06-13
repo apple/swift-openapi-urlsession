0bedb7532e6c4c31648eb5306b8147df26389bf2
Denil Chungath <95201442+denil-ct@users.noreply.github.com>
2023-06-13 17:32:58 +0530

    Update runtime minimum version to 0.1.3

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.1.3")),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
