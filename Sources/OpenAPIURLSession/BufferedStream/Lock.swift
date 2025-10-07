73620345260d1293b40d7c2d4af0a506065710cd
Si Beaumont <beaumont@apple.com>
2025-10-07 15:04:12 +0100

    remove swift-format-ignore-file from Lock.swift

--- Sources/OpenAPIURLSession/BufferedStream/Lock.swift
+++ Sources/OpenAPIURLSession/BufferedStream/Lock.swift
@@ -11,7 +11,6 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
-// swift-format-ignore-file
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the SwiftNIO open source project
